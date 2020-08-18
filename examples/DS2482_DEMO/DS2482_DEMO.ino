1c9057929329e0c017d193c7b3387489a60307ed
veraciousnottaken <veracious@wp.pl>
2020-08-18 20:51:01 +0200

    Cleanup code

--- examples/DS2482_DEMO/DS2482_DEMO.ino
+++ examples/DS2482_DEMO/DS2482_DEMO.ino
@@ -1,84 +1,123 @@
+#include <DS18B20_DS2482.h>
+#include <stdlib.h>
+#include "Arduino_DebugUtils.h"
+
+
+#define MAX_NUMBER_OF_SENSORS   16    // maximum number of Dallas sensors
+#define MAX_NUMBER_OF_CHANNELS  8     // 1=DS2482-100, 8=DS2482-800
+
+struct DSSensorStruct
+{
+  uint8_t Channel;
+  uint8_t Addr[8];
+  float Val;
+};
+
+// ADD SENSORS ONLY TO THE END OF LIST
+// ORDER IS USED IN VISUINO ANALOG ARRAY OUTPUT
+const char* DSSensors[MAX_NUMBER_OF_SENSORS][8] = {
+    {0x28, 0x56, 0xe1, 0x30, 0x00, 0x00, 0x00, 0xdd}, // T0
+    {0x28, 0xff, 0xe2, 0x47, 0x04, 0x00, 0x00, 0xe9}  // T1
+};
+
+DSSensorStruct Sensors[MAX_NUMBER_OF_SENSORS];
+DS2482 OneWireDemux(0);
+DS18B20_DS2482 DSTemperature(&OneWireDemux);
+uint8_t OneWireDemuxCount = 0;
+uint8_t DSCount = 0;
+
+// --------------------------------------------------------------------
+void setup() 
+{
+  Serial.begin(9600);  
+  Debug.setDebugLevel(DBG_DEBUG);
+  Debug.print(DBG_INFO, "\n--------- DS2482-x00 Test ----------------");
   
-//Tools
-  #include <Wire.h>             //I²C
+  // I2C
+  Debug.print(DBG_INFO, "Starting I2C");
+  OneWireDemux.Setup(2);
+  bool res = OneWireDemux.reset();
+  Debug.print(DBG_DEBUG, "DS2482 reset: %s", res ? "Ok" : "Error");
 
-//DS2482
-  #include <DS2482.h>
-  #include <DS18B20_DS2482.h>
-  DS2482 DS2482(0);   // 0 = 0x18
-  DS18B20_DS2482 DS18B20_devices(&DS2482);
-  DeviceAddress DS18B20_Address;
-  int DevicesCount = 0;
-  int TemperatueCount = 0;
-
-  unsigned long lastTemp = 0, tempScanInterval = 60 * 1000 * 1000;
+  //search for devices
+  OneWireDemuxCount = OneWireDemux.devicesCount(true);
+  Debug.print(DBG_INFO, "DS2482 devices: %d", OneWireDemuxCount);
   
-void setup() {
-  
-  Serial.begin(115200);
-  delay(1000);
-  Serial.println("\n\nDS2482-100 Test\n\n");
-  delay(1000);
+  for (uint8_t i=0; i<MAX_NUMBER_OF_CHANNELS; i++)
+  {
+    DSCount += ScanChannel(i, DSCount);
+  }
 
-  Serial.print("starting I2C: ");
-  Wire.begin(SDA, SCL);           //Start I²C
-  Serial.println("ok");
-  scanI2C();
-
-  Serial.print("DS2482-100 reset: ");
-  DS2482.reset();
-  Serial.println("ok");
-
-//search for devices and print address = true  
-  Serial.print("DS2482-100 scan: \n");
-  DevicesCount = DS2482.devicesCount(true);
-  Serial.println("devices: #" + (String)DevicesCount);
-
-  delay(10000);
-  Serial.print("DS18B20 begin: ");
-  DS18B20_devices.begin();
-  Serial.println("ok");
-  TemperatueCount = DS18B20_devices.getDeviceCount();
-  Serial.println("devices: #" + (String)TemperatueCount);
+  DSTemperature.setResolution(10);
 }
 
-void loop() {
-  unsigned long currMicros = micros();
-  unsigned long nextTemp;
+// --------------------------------------------------------------------
+uint8_t ScanChannel(uint8_t channel, uint8_t sensoroffset)
+{
+  bool res = OneWireDemux.selectChannel(channel);
+  Debug.print(DBG_DEBUG, "Channel%d selected: %s", channel, res ? "Ok" : "Error");
+  //delay(1000);
 
-  nextTemp = lastTemp + tempScanInterval;
-  delay(100);
-  
-  // temperature
-  if (nextTemp < currMicros) {
-    for (uint8_t i = 0; i < TemperatueCount; i++){
-      DS18B20_devices.requestTemperaturesByIndex(i);
-      Serial.print("temp #");
-      Serial.print(i);
-      Serial.print(": ");
-      Serial.println(DS18B20_devices.getTempCByIndex(i));
+  // 1-Wire
+  DSTemperature.begin();
+  uint8_t c = DSTemperature.getDeviceCount();
+  //Serial.println("Channel" + String(channel)+" DS18b20 devices: " + String(c));
+
+  for (uint8_t i=0; i<c; i++)
+  {
+    uint8_t index = sensoroffset + i;
+    DSTemperature.getAddress(Sensors[index].Addr, i);
+
+    Sensors[index].Channel = channel;
+    Debug.print(DBG_INFO, "Channel%d Sensor%d address: %s", channel, index, FormatAddress(index).c_str());
+  }
+
+  return c;
+}
+
+// --------------------------------------------------------------------
+String FormatAddress(uint8_t index)
+{
+    String addr = "";
+    for (uint8_t j=0; j<8; j++)
+    {
+      if (Sensors[index].Addr[j] < 0x10) addr += "0";
+      addr += String(Sensors[index].Addr[j], HEX);
+      if (j<7) addr += "-";
+    }
+    return addr;
+}
+
+// --------------------------------------------------------------------
+void loop() 
+{
+  // loop throught detected sensors
+  for (uint8_t i = 0; i < DSCount; i++)
+  {
+    OneWireDemux.selectChannel(Sensors[i].Channel);
+    DSTemperature.requestTemperaturesByAddress(Sensors[i].Addr);
+    Sensors[i].Val = DSTemperature.getTempC(Sensors[i].Addr);
+    if (Sensors[i].Val < -55.0 || Sensors[i].Val > 125.0) Sensors[i].Val = 255;
+    uint8_t j;
+    bool found = false;
+    
+    // locate sensor in declared sensors array (we know that for example index0=T_1, index1=T_2)
+    for (j = 0; j < MAX_NUMBER_OF_SENSORS; j++)
+    {
+        found = Sensors[i].Addr[0] == DSSensors[j][0] 
+            & Sensors[i].Addr[1] == DSSensors[j][1]
+            & Sensors[i].Addr[2] == DSSensors[j][2]
+            & Sensors[i].Addr[3] == DSSensors[j][3]
+            & Sensors[i].Addr[4] == DSSensors[j][4]
+            & Sensors[i].Addr[5] == DSSensors[j][5]
+            & Sensors[i].Addr[6] == DSSensors[j][6]
+            & Sensors[i].Addr[7] == DSSensors[j][7];
+        if (found) break;        
     }
 
-    lastTemp = currMicros;
+    if (!found) Debug.print(DBG_DEBUG, "Channel%d, %s: %s", Sensors[i].Channel, FormatAddress(i).c_str(), String(Sensors[i].Val, 3).c_str());
+    else Debug.print(DBG_DEBUG, "Channel%d, Sensor%d: %s", Sensors[i].Channel, j, String(Sensors[i].Val, 3).c_str());
   }
-}
 
-void scanI2C() {
-  for(byte address = 1; address < 127; address++ )
-  {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    byte error = Wire.endTransmission();
- 
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.print(address, HEX);
-      Serial.println("  !");
-    }    
-  }
+  delay(1000);
 }
